c83583ce0834be4ca0c567881616c79f78cb1ff2
hopskiprabbit <94173937+kabsmeiou@users.noreply.github.com>
2024-10-10 22:30:48 +0800

    fixed commit with .gitignore

---
+++ Cell Delivery/Assets/WinTrigger.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinTrigger : MonoBehaviour
+{
+    public GameObject winCanvas;
+    public GameObject timer;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {  // Make sure the player has the "Player" tag
+            timer.GetComponent<Timer>().StopTimer();
+            Debug.Log("Player reached the exit!");
+            // Add your win condition or level transition logic here
+            winCanvas.SetActive(true);
+        }
+    }
+}
