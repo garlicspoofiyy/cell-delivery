9b691d2b7e4e1570fd13835f4fe9d6310d485e5c
Selwyn Guiruela <garlicspoofiyyy@gmail.com>
2024-11-29 17:39:41 +0800

    add: animation to player
    - movement from idle, left, and right
    - bug is present where it loops to all of the animations when touch is true

--- Cell Delivery/Assets/Scripts/Shooting Game/Player.cs
+++ Cell Delivery/Assets/Scripts/Shooting Game/Player.cs
@@ -7,10 +7,12 @@ public class PlayerMovement : MonoBehaviour
     public float playerSpeed;
     private Vector2 playerDirection;
     private Rigidbody2D rb;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,12 +26,17 @@ public class PlayerMovement : MonoBehaviour
 
             // Smoothly move the player towards the touch position
             transform.position = Vector3.Lerp(transform.position, new Vector3(touchPosition.x, transform.position.y, transform.position.z), Time.deltaTime * playerSpeed);
+            playerDirection = new Vector2(touchPosition.x - transform.position.x, 0).normalized; // Update player direction
         }
         else
         {
             float directionX = Input.GetAxisRaw("Horizontal");
             playerDirection = new Vector2(directionX, 0).normalized;
         }
+
+        // Update animator parameters based on movement
+        animator.SetBool("isMoving", playerDirection.x != 0); // Set moving state
+        animator.SetFloat("moveDirection", playerDirection.x); // Set direction
     }
 
     private void FixedUpdate()
