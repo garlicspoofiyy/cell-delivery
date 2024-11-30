993af1b2118468af0cba1808baacae7b0e5f7d84
hopskiprabbit <94173937+kabsmeiou@users.noreply.github.com>
2024-11-30 14:09:29 +0800

    add: mazegame ui for win/lose

--- Cell Delivery/Assets/Scripts/Maze Game/WinTrigger.cs
+++ Cell Delivery/Assets/Scripts/Maze Game/WinTrigger.cs
@@ -7,36 +7,14 @@ using UnityEngine.SceneManagement;
 
 public class WinTrigger : MonoBehaviour
 {
-    public GameObject winCanvas;
     public GameObject timer;
-
-    public GameObject returnButton;
-    Canvas ResourceCanvas;
+    public GameOverScreen gameOverScreen;
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")) {  // Make sure the player has the "Player" tag
             timer.GetComponent<Timer>().StopTimer();
             Debug.Log("Player reached the exit!");
-            winCanvas.SetActive(true);
-            returnButton.SetActive(true);
+            gameOverScreen.Setup();
         }
-    }
-    
-    public void LoadOffice()
-    {
-        // find resource canvas and enable it
-        ResourceCanvas = GameObject.Find("ResourceCanvas").GetComponent<Canvas>();
-        ResourceCanvas.enabled = true;
-
-        Debug.Log("Current droplets: " + MainGameManager.droplets);
-        MainGameManager.droplets = Math.Min(MainGameManager.maxDropletsCapacity, MainGameManager.droplets + 10);
-        Debug.Log("Updated droplets: " + MainGameManager.droplets);
-
-        PlayerPrefs.SetInt("droplets", MainGameManager.droplets);
-        
-        Screen.orientation = ScreenOrientation.LandscapeLeft;
-        MainGameManager mainGameManager = FindObjectOfType<MainGameManager>();
-        mainGameManager.UpdateSliders();
-        SceneManager.LoadScene("Office");
     }
 }
--- Cell Delivery/Assets/Scripts/Misc/GameOver.cs
+++ Cell Delivery/Assets/Scripts/Misc/GameOver.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class GameOverScreen : MonoBehaviour
 {
+    Canvas ResourceCanvas;
     public void Setup() 
     {
         gameObject.SetActive(true);
@@ -13,6 +15,37 @@ public class GameOverScreen : MonoBehaviour
 
     public void Return()
     {
+        // find resource canvas and enable it
+        ResourceCanvas = GameObject.Find("ResourceCanvas").GetComponent<Canvas>();
+        ResourceCanvas.enabled = true;
+
+        Debug.Log("Current droplets: " + MainGameManager.droplets);
+        MainGameManager.droplets = Math.Max(MainGameManager.maxDropletsCapacity, MainGameManager.droplets - 10);
+        Debug.Log("Updated droplets: " + MainGameManager.droplets);
+
+        PlayerPrefs.SetInt("droplets", MainGameManager.droplets);
+        
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+        MainGameManager mainGameManager = FindObjectOfType<MainGameManager>();
+        mainGameManager.UpdateSliders();
+        SceneManager.LoadScene("Office");
+    }
+
+    public void ReturnOnWin() 
+    {
+        // find resource canvas and enable it
+        ResourceCanvas = GameObject.Find("ResourceCanvas").GetComponent<Canvas>();
+        ResourceCanvas.enabled = true;
+
+        Debug.Log("Current droplets: " + MainGameManager.droplets);
+        MainGameManager.droplets = Math.Min(MainGameManager.maxDropletsCapacity, MainGameManager.droplets + 10);
+        Debug.Log("Updated droplets: " + MainGameManager.droplets);
+
+        PlayerPrefs.SetInt("droplets", MainGameManager.droplets);
+        
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+        MainGameManager mainGameManager = FindObjectOfType<MainGameManager>();
+        mainGameManager.UpdateSliders();
         SceneManager.LoadScene("Office");
     }
 }
