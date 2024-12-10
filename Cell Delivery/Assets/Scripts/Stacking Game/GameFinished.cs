2b2c4f664bcc112e3b7f03092a7e589697f574e1
Selwyn Guiruela <garlicspoofiyyy@gmail.com>
2024-12-10 22:24:31 +0800

    fix: bugs
    - wait for the previous barrel to be destroyed before instantiating the next one
    - the game over screen will only pop up after the latest barrel is destroyed

--- Cell Delivery/Assets/Scripts/Stacking Game/GameFinished.cs
+++ Cell Delivery/Assets/Scripts/Stacking Game/GameFinished.cs
@@ -4,12 +4,13 @@ public class GameFinished : MonoBehaviour
 {
     public GameOverScreen GameOverScreenLose;
     public GameOverScreen GameOverScreenWin;
+    public static bool flag;
 
     void Update()
     {
         int count = CountInstances("PF Gate T1 Variant(Clone)");
 
-        if (AmmoSystem.parentTransform.childCount <= 0 && count > 0)
+        if (AmmoSystem.parentTransform.childCount <= 0 && count > 0 && flag == true)
         {
             GameOverLose();
         }
--- Cell Delivery/Assets/Scripts/Stacking Game/PlayerShoot.cs
+++ Cell Delivery/Assets/Scripts/Stacking Game/PlayerShoot.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerShoot : MonoBehaviour
 {
     public GameObject plateletPrefab;
     [SerializeField] float shootForce = 10f;
     private int ammo;
+    public static bool canShoot = true;
 
     private void Start()
     {
@@ -13,16 +15,18 @@ public class PlayerShoot : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && canShoot)
         {
-            Shoot();
+            StartCoroutine(Shoot());
         }
     }
 
-    void Shoot()
+    IEnumerator Shoot()
     {
         if (ammo > 0)
         {
+            canShoot = false;
+
             Debug.Log("Shooting platelet");
             Vector2 spawnPosition = new Vector2(transform.position.x, transform.position.y - 0.5f);
 
@@ -34,8 +38,14 @@ public class PlayerShoot : MonoBehaviour
             Rigidbody2D rb = platelet.GetComponent<Rigidbody2D>();
             rb.AddForce(-transform.up * shootForce, ForceMode2D.Impulse);
 
+            GameFinished.flag = false;
+
             ammo--;
+
+            // Wait for the platelet to be destroyed
+            yield return new WaitUntil(() => platelet == null);
+
+            canShoot = true;
         }
-        
     }
 }
